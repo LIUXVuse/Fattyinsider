370e58243ccaad02e581beeef308e7a7defbfcc5
LIUXVuse <liupony2000@gmail.com>
2025-03-15 16:32:53 +0800

    修改部署配置：使用installCommand替代buildCommand

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 安裝Python依賴
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # 顯示已安裝的包
-pip list
+python -m pip list
 
 # 確保目錄結構正確
 mkdir -p data/vector_store
